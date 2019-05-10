f440c2fbce6850dc0e3339b0b559efe593fddc1b
Ruby Hale <ruby@d3adspace.de>
2019-05-10 14:35:40 +0200

    Remove unused exception.

--- src/main/java/de/d3adspace/caladrius/exception/NoSuchCodecException.java
+++
@@ -1,8 +0,0 @@
-package de.d3adspace.caladrius.exception;
-
-public class NoSuchCodecException extends RuntimeException {
-
-    public NoSuchCodecException(String message) {
-        super(message);
-    }
-}
