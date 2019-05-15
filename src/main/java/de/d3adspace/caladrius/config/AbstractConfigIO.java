7073f34427ff80e43e0906ed34b68c911dc22587
Ruby Hale <ruby@d3adspace.de>
2019-05-15 19:05:19 +0200

    Add some java doc.

--- src/main/java/de/d3adspace/caladrius/config/AbstractConfigIO.java
+++ src/main/java/de/d3adspace/caladrius/config/AbstractConfigIO.java
@@ -4,20 +4,49 @@ import de.d3adspace.caladrius.Caladrius;
 
 import java.nio.file.Path;
 
+/**
+ * A config abstraction that focuses on providing pathed key support and providing the {@link Path} and
+ * {@link ConfigMeta<ContentObjectType>} to any config IO implementation.
+ *
+ * @param <ContentObjectType> The generic type of the config model.
+ */
 public abstract class AbstractConfigIO<ContentObjectType> {
 
+    /**
+     * The meta information of the config.
+     */
     private final ConfigMeta<ContentObjectType> configMeta;
+
+    /**
+     * The path where the config file should be located.
+     */
     private final Path path;
 
+    /**
+     * Create a new config io provider.
+     *
+     * @param configMeta The meta information of the config.
+     * @param path path where the config file should be located.
+     */
     public AbstractConfigIO(ConfigMeta<ContentObjectType> configMeta, Path path) {
         this.configMeta = configMeta;
         this.path = path;
     }
 
+    /**
+     * Get the path of the config.
+     *
+     * @return The path of the config.
+     */
     protected Path getPath() {
         return path;
     }
 
+    /**
+     * Get the meta information about the config.
+     *
+     * @return The meta information about the config.
+     */
     protected ConfigMeta<ContentObjectType> getConfigMeta() {
         return configMeta;
     }
